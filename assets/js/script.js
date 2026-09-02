c5d7dc3469c980a6941a7acdd1aaeb3732d6781f
BANNOStudio <angelobannister@gmail.com>
2026-09-02 15:52:53 +0200

    centering hero logo part 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,10 +44,6 @@
       // Adjust the speed value here
       mouseParallax(heroLogo, x, y, 5, parallaxBox); // Increase or decrease the number to change the effect
     };
-
-    parallaxBox.onmouseleave = function () {
-      heroLogo.style.transform = "none";
-    };
   };
 
   function mouseParallax(element, mouseX, mouseY, speed, box) {
